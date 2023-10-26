93ab43987c39384a6f89c8f2443a03d59299067f
Karol717 <karolklime77@wp.pl>
2023-10-26 14:25:15 +0200

    2

---
+++ 25.10.23/zad2.php
@@ -0,0 +1,19 @@
+<?php
+if(!isset ($_COOKIE[$cookie_name])) {
+    echo "Cookie named '" . $cookie_name . "' is not set!";
+
+} else {
+    echo "value is: ". $_COOKIE[$cookie_name];
+}
+?>
+</body>
+</html>
+<DOCTYPE html>
+  
+    <html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    </html>
