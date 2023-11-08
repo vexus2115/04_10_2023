d81992e223c60fb910581321a460f0c8dd4692a8
vexus2115 <19c.jakub.terelak@ezse.pl>
2023-11-08 14:51:55 +0100

    1

---
+++ 8_11_2023/zad1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a= 10;
+        $b= 11;
+
+        if($a > $b) {
+    echo"$a";
+        } else if($a < $b) {
+            echo "$b";
+        }
+        ?>
+</body>
+</html>
